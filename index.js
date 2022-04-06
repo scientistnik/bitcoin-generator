b61be3f60954de2264e42deefbf91ca95d85b230
Nikolay Nozdrin-Plotnitsky <scientistnik@yandex-team.ru>
2022-04-06 13:16:07 +0300

    catch SIGINT signal with logging

--- index.js
+++ index.js
@@ -2,34 +2,50 @@ import bitcoin from "bitcoinjs-lib";
 import sqlite3 from "sqlite3";
 import fs from "fs";
 
-const { Database } = sqlite3.verbose();
-
-const db = new Database("database.db");
-db.run("CREATE TABLE IF NOT EXISTS addresses (address TEXT, key TEXT)");
-
-const wantPrefixes = JSON.parse(fs.readFileSync("addresses.json").toString());
+var wantPrefixes = JSON.parse(fs.readFileSync("addresses.json").toString());
+var FORCE_EXIT = false;
 
 let currAddr = "";
 
 const findPrefix = (addr) =>
   wantPrefixes.find((want) => addr.slice(0, want.length).toLowerCase() == want);
 
-for (let i = 1; !findPrefix(currAddr); i++) {
-  const keyPair = bitcoin.ECPair.makeRandom();
-  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+const { Database } = sqlite3.verbose();
 
-  const privateKey = keyPair.toWIF();
+const db = new Database("database.db");
+db.run("CREATE TABLE IF NOT EXISTS addresses (address TEXT, key TEXT)", main);
 
-  if (findPrefix(address)) {
-	  var stmt = db.prepare("INSERT INTO addresses (address, key) VALUES (?, ?)");
-	  stmt.run(address, privateKey);
-	  stmt.finalize();
+const main = () => {
+  for (let i = 1; wantPrefixes.length != 0 && !FORCE_EXIT; i++) {
+    const keyPair = bitcoin.ECPair.makeRandom();
+    const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+
+    const privateKey = keyPair.toWIF();
+
+    const fAddr = findPrefix(address);
+    if (fAddr) {
+      var stmt = db.prepare(
+        "INSERT INTO addresses (address, key) VALUES (?, ?)"
+      );
+      stmt.run(address, privateKey);
+      stmt.finalize();
+
+      wantPrefixes = wantPrefixes.filter((addr) => addr != fAddr);
+      fs.writeFileSync("addresses.json", JSON.stringify(wantPrefixes));
+    }
+    currAddr = address;
+    if (i % 10000 == 0) console.log(`count=${i}`);
   }
-  currAddr = address;
-  if (i%10000 == 0)
-    console.log(`count=${i}`);
-}
 
-console.log(findPrefix(currAddr));
-db.close();
+  finish();
+};
 
+const finish = () => {
+  console.log(findPrefix(currAddr), FORCE_EXIT);
+  db.close();
+};
+
+process.on("SIGINT", () => {
+  console.log("catch SIGINT...");
+  FORCE_EXIT = true;
+});
