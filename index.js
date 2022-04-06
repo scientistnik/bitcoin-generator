9517aebab4acd3a1d142999868d883e360e21b51
Nikolay Nozdrin-Plotnitsky <scientistnik@yandex-team.ru>
2022-04-06 17:33:03 +0300

    catch SIGINT

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ import bitcoin from "bitcoinjs-lib";
 import sqlite3 from "sqlite3";
 import fs from "fs";
 
+const { Database } = sqlite3.verbose();
+
 var wantPrefixes = JSON.parse(fs.readFileSync("addresses.json").toString());
 var FORCE_EXIT = false;
+var i = 0;
 
 let currAddr = "";
 
@@ -15,38 +18,44 @@ process.on("SIGINT", () => {
   FORCE_EXIT = true;
 });
 
-const main = () => {
-  for (let i = 1; wantPrefixes.length != 0 && !FORCE_EXIT; i++) {
-    const keyPair = bitcoin.ECPair.makeRandom();
-    const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
+const cycle = () => {
+  const keyPair = bitcoin.ECPair.makeRandom();
+  const { address } = bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey });
 
-    const privateKey = keyPair.toWIF();
+  const privateKey = keyPair.toWIF();
 
-    const fAddr = findPrefix(address);
-    if (fAddr) {
-      var stmt = db.prepare(
-        "INSERT INTO addresses (address, key) VALUES (?, ?)"
-      );
-      stmt.run(address, privateKey);
-      stmt.finalize();
+  const fAddr = findPrefix(address);
+  if (fAddr) {
+    console.log(`find: ${fAddr} (index=${i + 1})`);
+    const db = new Database("database.db");
+    db.prepare("INSERT INTO addresses (address, key) VALUES (?, ?)")
+      .run(address, privateKey)
+      .finalize()
+      .close();
 
-      wantPrefixes = wantPrefixes.filter((addr) => addr != fAddr);
-      fs.writeFileSync("addresses.json", JSON.stringify(wantPrefixes));
-    }
-    currAddr = address;
-    if (i % 10000 == 0) console.log(`count=${i}`);
+    wantPrefixes = wantPrefixes.filter((addr) => addr != fAddr);
+    fs.writeFileSync("addresses.json", JSON.stringify(wantPrefixes));
   }
-
-  finish();
+  currAddr = address;
+  if (i % 10000 == 0) console.log(`count=${i}`);
+  i++;
 };
 
-const { Database } = sqlite3.verbose();
+const main = async () => {
+  if (wantPrefixes.length != 0 && !FORCE_EXIT) {
+    for (let j = 0; j < 1000; j++) cycle();
+  } else finish();
+
+  setTimeout(main, 0);
+};
 
 const db = new Database("database.db");
-db.run("CREATE TABLE IF NOT EXISTS addresses (address TEXT, key TEXT)", main);
+db.run("CREATE TABLE IF NOT EXISTS addresses (address TEXT, key TEXT)", () => {
+  db.close();
+  main();
+});
 
 const finish = () => {
-  console.log(findPrefix(currAddr), FORCE_EXIT);
-  db.close();
+  console.log(FORCE_EXIT);
+  process.exit(0);
 };
-
